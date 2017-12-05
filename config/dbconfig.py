2981d0622d6f7a13cc530038f23709ceca7c3578
jorgec <jorgec2013@gmail.com>
2017-12-04 20:01:28 -0500

    Initial commit

---
+++ config/dbconfig.py
@@ -0,0 +1,7 @@
+#Databse configuration information
+
+pg_config = {
+    'user' : '',
+    'passwd' : '',
+    'dbname' : ''
+}
---
+++ handler/supplier.py
@@ -0,0 +1,71 @@
+from flask import jsonify
+from dao.supplier import SupplierDAO
+
+
+class SupplierHandler:
+    def build_supplier_dict(self, row):
+        result = {}
+        result['sid'] = row[0]
+        result['sname'] = row[1]
+        result['scity'] = row[2]
+        result['sphone'] = row[3]
+        return result
+
+    def build_part_dict(self, row):
+        result = {}
+        result['pid'] = row[0]
+        result['pname'] = row[1]
+        result['pmaterial'] = row[2]
+        result['pcolor'] = row[3]
+        result['pprice'] = row[4]
+        result['quantity'] = row[5]
+        return result
+
+    def getAllSuppliers(self):
+
+        dao = SupplierDAO()
+        suppliers_list = dao.getAllSuppliers()
+        result_list = []
+        for row in suppliers_list:
+            result = self.build_supplier_dict(row)
+            result_list.append(result)
+        return jsonify(Suppliers=result_list)
+
+    def getSupplierById(self, sid):
+
+        dao = SupplierDAO()
+
+        row = dao.getSupplierById(sid)
+        if not row:
+            return jsonify(Error="Supplier Not Found"), 404
+        else:
+            part = self.build_supplier_dict(row)
+        return jsonify(Part=part)
+
+    def getPartsBySupplierId(self, sid):
+        dao = SupplierDAO()
+        if not dao.getSupplierById(sid):
+            return jsonify(Error="Supplier Not Found"), 404
+        parts_list = dao.getPartsBySupplierId(sid)
+        result_list = []
+        for row in parts_list:
+            result = self.build_part_dict(row)
+            result_list.append(result)
+        return jsonify(PartsSupply=result_list)
+
+    def searchSuppliers(self, args):
+        if len(args) > 1:
+            return jsonify(Error = "Malformed search string."), 400
+        else:
+            city = args.get("city")
+            if city:
+                dao = SupplierDAO()
+                supplier_list = dao.getSuppliersByCity(city)
+                result_list = []
+                for row in supplier_list:
+                    result = self.build_supplier_dict(row)
+                    result_list.append(row)
+                return jsonify(Suppliers=result_list)
+            else:
+                return jsonify(Error="Malformed search string."), 400
+
