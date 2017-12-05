af3b4ad0b025a430d2da82da4056a790069e164e
AlfredoMayFire <alfredo.pomales@upr.edu>
2017-12-05 13:08:12 -0400

    cositas

---
+++ handler/announcement.py
@@ -0,0 +1,11 @@
+from flask import jsonify
+
+class AnnouncementHandler:
+    def build_announcement_dict(self,row):
+        result{}
+        result['ANNid'] = row[0]
+        result['ANNdate'] = row[1]
+        return result
+
+    def getAllAnnouncements(self):
+        return result
