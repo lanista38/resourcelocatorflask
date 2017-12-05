6c3a78071b5e231ea3b2234d7d9181b898ceebf3
AlfredoMayFire <alfredo.pomales@upr.edu>
2017-12-05 15:00:49 -0400

    Rutas con handlers para operaciones hechos

--- main.py
+++ main.py
@@ -43,46 +43,23 @@ def getAllRequests():
 def getAllAnnouncements():
     return AnnouncementHandler().getAnnouncementByResource(Rname)
 
+@app.route('/ShowDashRequests/days/<int:days>/Region/<string:Region>')
+    return DashboardHandler().getDailyStatisticsByRequests(days,Region)
+
+@app.route('/ShowDashAnnouncements/days/<int:days>/Region/<string:Region>')
+    return DashboardHandler().getDailyStatisticsByAnnouncements(days,Region)
+
+@app.route('/ShowDashByMatches/days/<int:days>/Region/<string:Region>')
+    return DashboardHandler().getDailyStatisticsByMatches(days,Region)
+
+
 
 
 #Hasta aqui son nuestras rutas por ahora
 
 
 
-@app.route('/registerAdmin')
-def getAllParts():
-    if not request.args:
-        return RegistrationHandler().registerAdmin()
-    else:
-        return PartHandler().searchParts(request.args)
 
-
-@app.route('/PartApp/parts/<int:pid>')
-def getPartById(pid):
-    return PartHandler().getPartById(pid)
-
-@app.route('/PartApp/parts/<int:pid>/suppliers')
-def getSuppliersByPartId(pid):
-    return PartHandler().getSuppliersByPartId(pid)
-
-@app.route('/PartApp/suppliers')
-def getAllSuppliers():
-    if not request.args:
-        return SupplierHandler().getAllSuppliers()
-    else:
-        return SupplierHandler().searchSuppliers(request.args)
-
-@app.route('/PartApp/suppliers/<int:sid>')
-def getSupplierById(sid):
-    return SupplierHandler().getSupplierById(sid)
-
-@app.route('/PartApp/suppliers/<int:sid>/parts')
-def getPartsBySuplierId(sid):
-    return SupplierHandler().getPartsBySupplierId(sid)
-
-@app.route('/PartApp/requests')
-def getAllRequests():
-    return 0
 
 
 if __name__ == '__main__':
