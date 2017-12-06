1c8f80fb5343895fca35dde34befcf5e7c0adc48
jorgec <jorgec2013@gmail.com>
2017-12-05 19:57:09 -0500

    + handler w dictionary response

--- handler/resourceRequest.py
+++ handler/resourceRequest.py
@@ -1,18 +1,26 @@
 from flask import jsonify
 
 class RequestHandler:
+
     #Dictionary to be revised
-    def build_request_dict(self, row):
-        result = {}
-        result['Rid'] = row[0]
-        result['Rdate'] = row[1]
-        return result
+    def build_request_dict(self):
+        dict = {'Rid': '123', 'Rdate': 2017}
+        #result = {}
+        #result['Rid'] = row[0]
+        #result['Rdate'] = row[1]
+        return dict
 
     def getAllRequests(self):
-        return result
+
+        res = self.build_request_dict()
+        return jsonify(res)
 
     def getRequestByRPid(self, RPid):
-        return 0
+    
+        res = self.build_request_dict()
+        return jsonify(res)
 
     def getRequestByRid(self, Rid):
-        return 0
+
+        res = self.build_request_dict()
+        return jsonify(res)
