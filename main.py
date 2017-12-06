1dcd8c005d1b2accb8f033f5de0cc89a0945da68
A_Pomales3 <{alfredopomales@gmail.com}>
2017-12-05 22:36:46 -0400

    in process main page

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from handler.registration import RegistrationHandler
 from handler.dashboard import DashboardHandler
 from handler.announcement import AnnouncementHandler
 
-
+paragraph = '/ResourceLocator/suppliers /n /ResourceLocator/suppliers/<int:sid> /n /ResourceLocator/requests/<int:Rid> /n'
+paragraph1 = '/ResourceLocator/requests/<int:RPid> /n /ResourceLocator/purchases /n /ResourceLocator/purchases/<int:Rid> /n'
+paragraph2 = '/ResourceLocator/purchases/<int:RPid> /n /ResourceLocator/registerAdmin /n /ResourceLocator/registerSupplier /n'
+paragraph3 = '/ResourceLocator/registerRequester /n '
 
 app = Flask(__name__)
 
 @app.route('/')
 def greeting():
-    return 'Hello, this is the parts DB App!'
+    return ''
 
 
 @app.route('/ResourceLocator/suppliers')
