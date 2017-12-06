66c09fed3a5e1a2cdd6e33e38ab8a930d01a22ff
davidriquelmer <34255430+davidriquelmer@users.noreply.github.com>
2017-12-05 22:38:21 -0400

    CATEGORY RESOURCE HANDLER

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
-from handler.parts import PartHandler
+
+from handler.category import CategoryHandler
+from handler.resource import ResourceHandler
 from handler.supplier import SupplierHandler
+from handler.resourceRequest import RequestHandler
+from handler.purchaseReserve import PurchaseHandler
 
 
 
@@ -10,20 +14,6 @@ app = Flask(__name__)
 def greeting():
     return 'Hello, this is the parts DB App!'
 
-@app.route('/PartApp/parts')
-def getAllParts():
-    if not request.args:
-        return PartHandler().getAllParts()
-    else:
-        return PartHandler().searchParts(request.args)
-
-@app.route('/PartApp/parts/<int:pid>')
-def getPartById(pid):
-    return PartHandler().getPartById(pid)
-
-@app.route('/PartApp/parts/<int:pid>/suppliers')
-def getSuppliersByPartId(pid):
-    return PartHandler().getSuppliersByPartId(pid)
 
 @app.route('/PartApp/suppliers')
 def getAllSuppliers():
@@ -36,9 +26,65 @@ def getAllSuppliers():
 def getSupplierById(sid):
     return SupplierHandler().getSupplierById(sid)
 
-@app.route('/PartApp/suppliers/<int:sid>/parts')
-def getPartsBySuplierId(sid):
-    return SupplierHandler().getPartsBySupplierId(sid)
+
+# Resource Requests routes
+@app.route('/PartApp/requests')
+def getAllRequests():
+    return RequestHandler().getAllRequests()
+
+@app.route('/PartApp/requests/<int:Rid>')
+def getRequestById(Rid):
+    return RequestHandler().getRequestByRid(Rid)
+
+@app.route('/PartApp/requests/<int:RPid>')
+def getRequestByRPId(RPid):
+    return RequestHandler().getRequestByRPId(RPid)
+
+
+#Reserve/Purchase routes
+
+@app.route('/PartApp/purchases')
+def getAllPurchases():
+    return PurchaseHandler().getAllPurchases()
+
+@app.route('/PartApp/purchases/<int:Rid>')
+def getPurchaseByRid(Rid):
+    return PurchaseHandler().getPurchaseByRid(Rid)
+
+@app.route('/PartApp/purchases/<int:RPid>')
+def getPurchaseByRPid(RPid):
+    return PurchaseHandler().getPurchaseByRPid(RPid)
+
+#Category routes
+
+@app.route('/PartApp/category')
+def getAllCategories():
+    return CategoryHandler().getAllCategories()
+
+@app.route('/PartApp/category/<int:Cid>')
+def getCategoryByCid(Cid):
+    return CategoryHandler().getCategoryByCid(Cid)
+
+@app.route('/PartApp/category/<string:name>')
+def getCategoryByCName(name):
+    return CategoryHandler().getCategoryByCname(name)
+
+#Resource routes
+
+@app.route('/PartApp/resource')
+def getAllResources():
+    if not request.args:
+        return ResourceHandler().getAllResources()
+    else:
+        return ResourceHandler().search(request.args)
+
+@app.route('/PartApp/resource/<int:Rid>')
+def getResourceByRid(Rid):
+    return ResourceHandler().getResourceByRid(Rid)
+
+@app.route('/PartApp/resource/<string:name>')
+def getResourceByName(name):
+    return ResourceHandler().getResourceByName(name)
 
 if __name__ == '__main__':
     app.run()
